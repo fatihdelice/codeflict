ae08cf0c226633e32d5f264c578eb8624793d8bb
fatihdelice <delicex58@gmail.com>
2024-04-13 03:29:26 +0300

    feat: mobile menu responsive container bug fixed

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -4,7 +4,7 @@ import MobileHeaderLogo from "./MobileHeaderLogo"
 import ThemeSwitch from "./ThemeSwitch"
 import Link from 'next/link'
 
-export default function MobileMenu({toggleMobileMenu}) {
+export default function MobileMenu({ toggleMobileMenu }) {
     return (
         <div className="fixed top-0 left-0 w-full flex-col flex h-full bg-white dark:bg-black z-40">
             <header className="py-2 mb-8 md:mb-24 border-b-2 border-black dark:border-white">
@@ -17,15 +17,17 @@ export default function MobileMenu({toggleMobileMenu}) {
                 </Container>
             </header>
             <div className="flex-1 overflow-auto p-6 border-t border-white/10">
-                <nav className="grid gap-y-4">
-                    <Link href="/about" className="flex items-center text-2xl py-1.5 px-4">
-                        <span>About</span>
-                    </Link>
-                    <Link href="/products" className="flex items-center text-2xl py-1.5 px-4">
-                        <span>Products</span>
-                        <span className="ml-1 px-2 py-0.5 animate-pulse flex rounded-full bg-gray-700 text-xs tracking-wider dark:bg-white/10 text-green-400 dark:text-orange-300"> new</span>
-                    </Link>
-                </nav>
+                <Container>
+                    <nav className="grid gap-y-4">
+                        <Link href="/about" className="flex items-center text-2xl py-1.5 px-4">
+                            <span>About</span>
+                        </Link>
+                        <Link href="/products" className="flex items-center text-2xl py-1.5 px-4">
+                            <span>Products</span>
+                            <span className="ml-1 px-2 py-0.5 animate-pulse flex rounded-full bg-gray-700 text-xs tracking-wider dark:bg-white/10 text-green-400 dark:text-orange-300"> new</span>
+                        </Link>
+                    </nav>
+                </Container>
             </div>
         </div>
     )
