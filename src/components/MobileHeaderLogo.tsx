4a191754ced99d229abddcec1cd3457ec95079e0
fatihdelice <delicex58@gmail.com>
2024-04-13 03:16:24 +0300

    feat: responsive bug fixed

--- src/components/MobileHeaderLogo.tsx
+++ src/components/MobileHeaderLogo.tsx
@@ -1,8 +1,8 @@
 export default function MobileHeaderLogo({ toggleMobileMenu }) {
     return (
-        <button className="pr-2 md:hidden" onClick={toggleMobileMenu}>
-            <svg stroke="currentColor" fill="none" stroke-width="0" viewBox="0 0 24 24" height="26" width="26" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7"></path>
+        <button className="pr-2 lg:hidden" onClick={toggleMobileMenu}>
+            <svg stroke="currentColor" fill="none" strokeWidth="0" viewBox="0 0 24 24" height="26" width="26" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7"></path>
             </svg>
         </button>
     )
